abc490b0fbea7f905ab8d5153b30cb1f31cdc474
Niz46 <paujohn66@gmail.com>
2025-01-30 07:42:51 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+function updateUTCTime() {
+    const utcTime = new Date().toUTCString();
+    document.getElementById("utc-time").textContent = utcTime;
+}
+
+setInterval(updateUTCTime, 1000);
+
+updateUTCTime();
